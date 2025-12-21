8a1208965d311c1ac7f47bf9baca72652ab35846
benbencik <bencik42@gmail.com>
2025-12-21 22:33:47 +0100

    update script for waybar

--- theming/update-waybar.sh
+++ theming/update-waybar.sh
@@ -15,17 +15,19 @@ hex_to_rgba() {
 }
 
 # Read colors from the theme file
-foreground=$(grep '^foreground' $THEME_FILE | sed 's/.*#/#/')
-background_hex=$(grep '^background' $THEME_FILE | sed 's/.*#/#/')
-background_alt_hex=$(grep '^selection_background' $THEME_FILE | sed 's/.*#/#/')
+foreground=$(grep '^foreground' "$THEME_FILE" | awk '{print $2}')
+background_hex=$(grep '^background' "$THEME_FILE" | awk '{print $2}')
+selection_background_hex=$(grep '^selection_background' "$THEME_FILE" | awk '{print $2}')
+color2=$(grep '^color2' "$THEME_FILE" | awk '{print $2}')  # green - accent
 
 # Convert hex colors to rgba
-background=$(hex_to_rgba $background_hex 0.9)
-background_alt=$(hex_to_rgba $background_alt_hex 0.9)
+background=$(hex_to_rgba "$background_hex" 0.95)
+background_alt="$selection_background_hex"
 
 # Update the style.css file
-sed -i "s/@define-color foreground .*/@define-color foreground $foreground;/" $STYLE_FILE
-sed -i "s/@define-color background .*/@define-color background $background;/" $STYLE_FILE
-sed -i "s/@define-color background-alt .*/@define-color background-alt $background_alt;/" $STYLE_FILE
+sed -i "s/@define-color foreground .*/@define-color foreground $foreground;/" "$STYLE_FILE"
+sed -i "s/@define-color background .*/@define-color background $background;/" "$STYLE_FILE"
+sed -i "s/@define-color background-alt .*/@define-color background-alt $background_alt;/" "$STYLE_FILE"
+sed -i "s/@define-color accent .*/@define-color accent $color2;/" "$STYLE_FILE"
 
 echo "Waybar style updated with current theme colors."
