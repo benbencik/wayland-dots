98bb540cb233ebffc724ce654b205982f432db76
benbencik <bencik42@gmail.com>
2026-07-29 15:57:34 +0200

    more reliable 2x2 grid

--- home-manager/startup_terminal.sh
+++ home-manager/startup_terminal.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
 CURRENT_WS=$(hyprctl activeworkspace | awk '/workspace ID/ { print $3 }')
-kitty --class="grid_terminal" --title="top_left" -- bluetoothctl &
+
+hyprctl dispatch workspace 8
+sleep 0.3
+
+kitty --class="grid_terminal" --title="top_left" -- bluetuith &
 sleep 0.5
-kitty --class="grid_terminal" --title="top_right" -- wiremix &  
+
+hyprctl dispatch layoutmsg preselect right
+kitty --class="grid_terminal" --title="top_right" -- wiremix &
 sleep 0.5
+
+hyprctl dispatch focuswindow title:^top_left$
+sleep 0.2
+hyprctl dispatch layoutmsg preselect down
 kitty --class="grid_terminal" --title="bottom_left" --hold -- ~/.config/home-manager/hello-duck.sh &
 sleep 0.5
-hyprctl dispatch focuswindow title:^top_right$ 
-sleep 0.5
+
+hyprctl dispatch focuswindow title:^top_right$
+sleep 0.2
+hyprctl dispatch layoutmsg preselect down
 kitty --class="grid_terminal" --title="bottom_right" &
-sleep 0.5
+sleep 0.3
+
 hyprctl dispatch workspace $CURRENT_WS
